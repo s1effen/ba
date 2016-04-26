12648d15c1abb72b47afe8c3627482f9f4a1c980
Steffen <steffen@dhcp-172-21-20-91.wlan.uni-hamburg.de.local>
2016-04-26 10:51:16 +0200

    Init

---
+++ animals/Assets/Hammer.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hammer : MonoBehaviour {
+
+	public GameObject rotateAround;
+	public GameObject hammer;
+	public bool mouseControl = true;
+	public float mouseSpeed = 0.05f;
+	public bool hit;
+	private Animator anim;
+
+
+	// Use this for initialization
+	void Start () {
+		anim = hammer.GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		movement ();
+		hit = hitObject();
+	}
+
+	bool hitObject()
+	{
+		if (!mouseControl)
+			return false;
+		if (Input.GetMouseButtonDown (0))
+		{
+			anim.SetTrigger("hit");
+			Debug.Log("hiiiiit");
+			return true;
+		}
+
+		return false;
+	}
+
+
+
+	void movement()
+	{
+		if (!mouseControl)
+			return;
+		
+		float h = mouseSpeed * Input.GetAxis("Mouse X");
+		float v = mouseSpeed * Input.GetAxis("Mouse Y");
+		if(h != 0 || v != 0)
+			transform.RotateAround(rotateAround.transform.position, Vector3.up, 40 * h);
+			//transform.position += transform.forward * v;
+			transform.position = transform.position + new Vector3(h,0,v); 
+	}
+}
---
+++ animals/Assets/RunAway.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RunAway : MonoBehaviour {
+
+	public GameObject fearObject;
+	private Hammer hammer; 
+	private Animator anim;
+	private float walkSpeed = 2.0f;
+	private bool jumpActive=false;
+	Vector3 basePosition;
+	float jumpSpeed = 1f;
+	Vector3 fearObjectPos;
+
+
+
+	// Use this for initialization
+	void Start () {
+		//fearObjectTransform = new Transform();
+		hammer = fearObject.GetComponentInParent<Hammer> ();
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		//Jump if hammer is hit
+		if (hammer.hit || Input.GetKeyDown(KeyCode.Space))
+		{
+			//turn away from object
+			fearObjectPos = new Vector3(fearObject.transform.position.x,transform.position.y,fearObject.transform.position.z);
+			transform.LookAt (fearObjectPos);
+			transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x,transform.rotation.eulerAngles.y -180,transform.rotation.eulerAngles.z);
+
+			//Jump
+			anim.SetTrigger("Jump");
+		}
+
+		if (jumpActive)
+		{
+			//Move up
+			transform.position = basePosition + Vector3.up * anim.GetFloat("Up");
+
+			//Move Forward
+			basePosition += transform.forward * jumpSpeed * Time.deltaTime;
+		}
+
+	}
+
+	//Called by Animation at current Screen
+	void JumpStart()
+	{
+		basePosition = transform.position;
+		jumpActive = true;
+		Debug.Log ("Jump started");
+	}
+
+	//Called by Animation at current Screen
+	void JumpEnd()
+	{
+		jumpActive = false;
+		Debug.Log ("Jump ended");
+	}
+
+}
